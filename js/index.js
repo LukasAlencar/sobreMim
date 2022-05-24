1a8c01722718bda77f2acbb724af557a374323c9
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-05-24 02:59:30 -0300

    Criando meu site

---
+++ js/index.js
@@ -0,0 +1,55 @@
+function enterNav(){
+    var labels = window.document.querySelector('.instaLabels');
+    labels.style.visibility = "visible";
+    labels.style.opacity = '100';
+
+}
+function leaveNav(){
+    var labels = window.document.querySelector('.instaLabels');
+    
+    labels.style.opacity = "0";
+    labels.style.visibility = "hidden";
+}
+
+function enterGithubNav(){
+    var labelsGit = window.document.querySelector('.githubLabels');
+    
+    labelsGit.style.visibility = "visible";
+    labelsGit.style.opacity = '100';
+
+}
+function leaveGithubNav(){
+    var labelsGit = window.document.querySelector('.githubLabels');
+
+    labelsGit.style.opacity = "0";
+    labelsGit.style.visibility = "hidden";
+}
+
+function enterYoutubeNav(){
+    var labelsGit = window.document.querySelector('.youtubeLabels');
+    
+    labelsGit.style.visibility = "visible";
+    labelsGit.style.opacity = '100';
+
+}
+function leaveYoutubeNav(){
+    var labelsGit = window.document.querySelector('.youtubeLabels');
+
+    labelsGit.style.opacity = "0";
+    labelsGit.style.visibility = "hidden";
+}
+
+
+function enterLinkedinNav(){
+    var labelsGit = window.document.querySelector('.linkedinLabels');
+    
+    labelsGit.style.visibility = "visible";
+    labelsGit.style.opacity = '100';
+
+}
+function leaveLinkedinNav(){
+    var labelsGit = window.document.querySelector('.linkedinLabels');
+
+    labelsGit.style.opacity = "0";
+    labelsGit.style.visibility = "hidden";
+}
